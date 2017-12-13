5ba3af27a701a7f97c6e5f0b43bc0e335516f0bf
David Lievrouw <dlievrouw2@csc.com>
2017-12-13 08:47:18 +0100

    Add empty Scaffolding solution for dotnetcore

---
+++ src/Motorsports.Scaffolding.Core/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Motorsports.Scaffolding.Core.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
