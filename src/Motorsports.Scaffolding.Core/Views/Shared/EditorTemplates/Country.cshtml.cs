97263157aed683d80773beb30b921268bf8bf6ca
David Lievrouw <dlievrouw2@csc.com>
2017-12-17 16:49:58 +0100

    Use empty EditorTemplate for country selection for Participants.

---
+++ src/Motorsports.Scaffolding.Core/Views/Shared/EditorTemplates/Country.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Motorsports.Scaffolding.Core.Views.Shared
+{
+    public class CountryModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
