01611baf1dd5a565d4a6f399b8a41e35d01798c3
David Lievrouw <dlievrouw2@csc.com>
2017-12-18 16:27:25 +0100

    Add SeasonsController.

---
+++ src/Motorsports.Scaffolding.Core/Controllers/SeasonsController.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Motorsports.Scaffolding.Core.Models;
+
+namespace Motorsports.Scaffolding.Core.Controllers {
+  public class SeasonsController : Controller {
+    readonly MotorsportsContext _context;
+
+    public SeasonsController(MotorsportsContext context) {
+      _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    // GET: Seasons
+    public async Task<IActionResult> Index() {
+      var motorsportsContext = _context.Season
+        .Include(s => s.RelatedSport)
+        .OrderBy(s => s.NiceLabel);
+      return View(await motorsportsContext.ToListAsync());
+    }
+
+    // GET: Seasons/Details/5
+    public async Task<IActionResult> Details(int? id) {
+      if (id == null) return NotFound();
+
+      var season = await _context.Season
+        .Include(s => s.RelatedSport)
+        .SingleOrDefaultAsync(m => m.Id == id);
+      if (season == null) return NotFound();
+
+      return View(season);
+    }
+
+    // GET: Seasons/Create
+    public IActionResult Create() {
+      ViewData["Sport"] = new SelectList(_context.Sport, "Name", "Name");
+      return View(new Season());
+    }
+
+    // POST: Seasons/Create
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Sport,Label")] Season season) {
+      if (ModelState.IsValid) {
+        _context.Add(season);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+      }
+      ViewData["Sport"] = new SelectList(_context.Sport, "Name", "Name", season.Sport);
+      return View(season);
+    }
+
+    // GET: Seasons/Edit/5
+    public async Task<IActionResult> Edit(int? id) {
+      if (id == null) return NotFound();
+
+      var season = await _context.Season.SingleOrDefaultAsync(m => m.Id == id);
+      if (season == null) return NotFound();
+      ViewData["Sport"] = new SelectList(_context.Sport, "Name", "Name", season.Sport);
+      return View(season);
+    }
+
+    // POST: Seasons/Edit/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(int id, [Bind("Id,Sport,Label")] Season season) {
+      if (id != season.Id) return NotFound();
+
+      if (ModelState.IsValid) {
+        try {
+          _context.Update(season);
+          await _context.SaveChangesAsync();
+        } catch (DbUpdateConcurrencyException) {
+          if (!SeasonExists(season.Id)) return NotFound();
+          else throw;
+        }
+        return RedirectToAction(nameof(Index));
+      }
+      ViewData["Sport"] = new SelectList(_context.Sport, "Name", "Name", season.Sport);
+      return View(season);
+    }
+
+    // GET: Seasons/Delete/5
+    public async Task<IActionResult> Delete(int? id) {
+      if (id == null) return NotFound();
+
+      var season = await _context.Season
+        .Include(s => s.RelatedSport)
+        .SingleOrDefaultAsync(m => m.Id == id);
+      if (season == null) return NotFound();
+
+      return View(season);
+    }
+
+    // POST: Seasons/Delete/5
+    [HttpPost]
+    [ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id) {
+      var season = await _context.Season.SingleOrDefaultAsync(m => m.Id == id);
+      _context.Season.Remove(season);
+      await _context.SaveChangesAsync();
+      return RedirectToAction(nameof(Index));
+    }
+
+    bool SeasonExists(int id) {
+      return _context.Season.Any(e => e.Id == id);
+    }
+  }
+}
---
+++ src/Motorsports.Scaffolding.Core/Models/Extensions/Season.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel;
+using System.Linq;
+
+namespace Motorsports.Scaffolding.Core.Models {
+  public partial class Season {
+    [DisplayName("Nice label")]
+    public string NiceLabel {
+      get {
+        if (!string.IsNullOrWhiteSpace(Label)) return $"{Label} ({Sport})";
+        var firstRound = RelatedRounds?.OrderBy(r => r.Number)?.FirstOrDefault()?.Date;
+        var lastRound = RelatedRounds?.OrderByDescending(r => r.Number)?.FirstOrDefault()?.Date;
+        var firstYear = firstRound?.Year;
+        var lastYear = lastRound?.Year;
+        if (firstYear.HasValue && lastYear.HasValue) {
+          return firstYear.Value == lastYear.Value
+            ? $"{Sport} ({firstYear.Value})"
+            : $"{Sport} ({firstYear.Value}-{lastYear.Value})";
+        }
+        return $"{Sport} (no rounds defined)";
+      }
+    }
+  }
+}
