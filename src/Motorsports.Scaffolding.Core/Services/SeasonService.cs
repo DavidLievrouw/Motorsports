13c71cb865156e58642b11ce13826671e997590a
David Lievrouw <dlievrouw2@csc.com>
2017-12-23 16:58:09 +0100

    Allow specifying the winning team when editing a season.

---
+++ src/Motorsports.Scaffolding.Core/Services/SeasonService.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using Motorsports.Scaffolding.Core.Models;
+using Motorsports.Scaffolding.Core.Models.UpdateModels;
+
+namespace Motorsports.Scaffolding.Core.Services {
+  public interface ISeasonService {
+    Task UpdateSeason(SeasonUpdateModel season);
+  }
+
+  public class SeasonService : ISeasonService {
+    readonly MotorsportsContext _context;
+
+    public SeasonService(MotorsportsContext context) {
+      _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public async Task UpdateSeason(SeasonUpdateModel season) {
+      if (season == null) throw new ArgumentNullException(nameof(season));
+
+      var seasonToUpdate = _context.Season.Single(s => s.Id == season.Id);
+      if (season.WinningTeamId.HasValue) {
+        seasonToUpdate.RelatedSeasonResult = new SeasonResult {
+          Season = season.Id,
+          WinningTeam = season.WinningTeamId.Value
+        };
+      }
+      else _context.SeasonResult.RemoveRange(_context.SeasonResult.Where(sr => sr.Season == season.Id));
+      _context.Update(seasonToUpdate);
+      await _context.SaveChangesAsync();
+    }
+  }
+}
