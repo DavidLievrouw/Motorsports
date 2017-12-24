2a1fc9cc1640688a59421b7cf5f6943d8d54431c
David Lievrouw <dlievrouw2@csc.com>
2017-12-24 15:44:56 +0100

    Rename Team edittemplate to WinningTeam.

--- src/Motorsports.Scaffolding.Core/Views/Shared/EditorTemplates/WinningTeam.cshtml.cs
+++ src/Motorsports.Scaffolding.Core/Views/Shared/EditorTemplates/WinningTeam.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Motorsports.Scaffolding.Core.Views.Shared {
-  public class TeamModel : PageModel {
+  public class WinningTeamModel : PageModel {
     public void OnGet() { }
   }
 }
