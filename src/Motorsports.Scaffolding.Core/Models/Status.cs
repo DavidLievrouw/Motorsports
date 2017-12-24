dc17bc6cd76a9a5e4252a29cb7b3a827dedd4f47
David Lievrouw <dlievrouw2@csc.com>
2017-12-24 11:07:07 +0100

    Wire up initial version of the Rounds UI.

--- src/Motorsports.Scaffolding.Core/Models/Status.cs
+++ src/Motorsports.Scaffolding.Core/Models/Status.cs
@@ -1,5 +1,13 @@
-﻿namespace Motorsports.Scaffolding.Core.Models {
+﻿using System.Collections.Generic;
+
+namespace Motorsports.Scaffolding.Core.Models {
   public partial class Status {
+    public Status() {
+      RelatedRoundResults = new HashSet<RoundResult>();
+    }
+
     public string Name { get; set; }
+
+    public ICollection<RoundResult> RelatedRoundResults { get; set; }
   }
 }
