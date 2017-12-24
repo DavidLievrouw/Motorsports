fa77ddf0a03033159ae1640170f6bba8c8eedeed
David Lievrouw <dlievrouw2@csc.com>
2017-12-24 10:51:27 +0100

    Add additional methods to the RoundService.

--- src/Motorsports.Scaffolding.Core/Services/RoundService.cs
+++ src/Motorsports.Scaffolding.Core/Services/RoundService.cs
@@ -1,13 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using Motorsports.Scaffolding.Core.Models;
+using Motorsports.Scaffolding.Core.Models.DisplayModels;
 using Motorsports.Scaffolding.Core.Models.EditModels;
 
 namespace Motorsports.Scaffolding.Core.Services {
   public interface IRoundService {
+    Task<RoundDisplayModel> GetNew();
+    Task<List<RoundDisplayModel>> LoadRoundList();
+    Task<RoundDisplayModel> LoadDisplayModel(int roundId);
     Task UpdateRound(RoundEditModel round);
+    Task CreateRound(Round round);
+    Task DeleteRound(int roundId);
   }
 
   public class RoundService : IRoundService {
@@ -15,6 +22,44 @@ namespace Motorsports.Scaffolding.Core.Services {
 
     public RoundService(MotorsportsContext context) {
       _context = context ?? throw new ArgumentNullException(nameof(context));
+    }
+
+    public Task<RoundDisplayModel> GetNew() {
+      return Task.FromResult(
+        new RoundDisplayModel(
+          new Round(),
+          _context.Season.Include(s => s.RelatedRounds).OrderBy(season => season.Sport).ThenBy(season => season.RelatedRounds.Min(r => r.Date)),
+          _context.Team.OrderBy(team => team.Sport).ThenBy(team => team.Name),
+          _context.Participant.OrderBy(participant => participant.LastName).ThenBy(participant => participant.FirstName),
+          _context.Status));
+    }
+
+    public Task<List<RoundDisplayModel>> LoadRoundList() {
+      return _context.Round
+        .Include(r => r.RelatedSeason)
+        .Include(r => r.RelatedRoundResult)
+        .ThenInclude(rr => rr.RelatedWinningTeam)
+        .Include(r => r.RelatedRoundWinners)
+        .ThenInclude(rw => rw.RelatedParticipant)
+        .Select(r => new RoundDisplayModel(r, null, null, null, null))
+        .ToListAsync();
+    }
+
+    public async Task<RoundDisplayModel> LoadDisplayModel(int roundId) {
+      var seasonDataModel = await _context.Round
+        .Include(r => r.RelatedSeason)
+        .Include(r => r.RelatedRoundResult)
+        .ThenInclude(rr => rr.RelatedWinningTeam)
+        .Include(r => r.RelatedRoundWinners)
+        .ThenInclude(rw => rw.RelatedParticipant)
+        .SingleOrDefaultAsync(m => m.Id == roundId);
+
+      return new RoundDisplayModel(
+        seasonDataModel,
+        _context.Season.Include(s => s.RelatedRounds).OrderBy(season => season.Sport).ThenBy(season => season.RelatedRounds.Min(r => r.Date)),
+        _context.Team.OrderBy(team => team.Sport).ThenBy(team => team.Name),
+        _context.Participant.OrderBy(participant => participant.LastName).ThenBy(participant => participant.FirstName),
+        _context.Status);
     }
 
     public async Task UpdateRound(RoundEditModel round) {
@@ -29,7 +74,10 @@ namespace Motorsports.Scaffolding.Core.Services {
       // Update round result
       if (roundToUpdate.RelatedRoundResult == null && round.WinningTeamId.HasValue ||
           roundToUpdate.RelatedRoundResult?.WinningTeam != round.WinningTeamId ||
-          roundToUpdate.RelatedRoundResult?.Rain != (round.Rain.HasValue? (decimal) round.Rain: new decimal?()) ||
+          roundToUpdate.RelatedRoundResult?.Rain !=
+          (round.Rain.HasValue
+            ? (decimal) round.Rain
+            : new decimal?()) ||
           roundToUpdate.RelatedRoundResult?.Status != round.Status?.ToString() ||
           roundToUpdate.RelatedRoundResult?.Rating != round.Rating) {
         _context.RoundResult.RemoveRange(_context.RoundResult.Where(sr => sr.Round == round.Id));
@@ -68,6 +116,17 @@ namespace Motorsports.Scaffolding.Core.Services {
 
       // Commit
       _context.Update(roundToUpdate);
+      await _context.SaveChangesAsync();
+    }
+
+    public async Task CreateRound(Round round) {
+      _context.Add(round);
+      await _context.SaveChangesAsync();
+    }
+
+    public async Task DeleteRound(int roundId) {
+      var round = await _context.Round.SingleOrDefaultAsync(m => m.Id == roundId);
+      _context.Round.Remove(round);
       await _context.SaveChangesAsync();
     }
   }
