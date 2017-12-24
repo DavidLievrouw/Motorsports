c14fe484e75325c44784e27d38e8fb69561d594d
David Lievrouw <dlievrouw2@csc.com>
2017-12-24 10:30:09 +0100

    Scaffold basic infrastructure for Rounds controller.

---
+++ src/Motorsports.Scaffolding.Core/Controllers/RoundsController.cs
@@ -0,0 +1,128 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Motorsports.Scaffolding.Core.Models;
+
+namespace Motorsports.Scaffolding.Core.Controllers {
+  public class RoundsController : Controller {
+    readonly MotorsportsContext _context;
+
+    public RoundsController(MotorsportsContext context) {
+      _context = context;
+    }
+
+    // GET: Rounds
+    public async Task<IActionResult> Index() {
+      var motorsportsContext = _context.Round.Include(r => r.RelatedSeason).Include(r => r.RelatedVenue);
+      return View(await motorsportsContext.ToListAsync());
+    }
+
+    // GET: Rounds/Details/5
+    public async Task<IActionResult> Details(int? id) {
+      if (id == null) return NotFound();
+
+      var round = await _context.Round
+        .Include(r => r.RelatedSeason)
+        .Include(r => r.RelatedVenue)
+        .SingleOrDefaultAsync(m => m.Id == id);
+      if (round == null) return NotFound();
+
+      return View(round);
+    }
+
+    // GET: Rounds/Create
+    public IActionResult Create() {
+      ViewData["Season"] = new SelectList(_context.Season, "Id", "Sport");
+      ViewData["Venue"] = new SelectList(_context.Venue, "Name", "Name");
+      return View(new Round());
+    }
+
+    // POST: Rounds/Create
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create(
+      [Bind("Id,Date,Number,Name,Season,Venue")]
+      Round round) {
+      if (ModelState.IsValid) {
+        _context.Add(round);
+        await _context.SaveChangesAsync();
+        return RedirectToAction(nameof(Index));
+      }
+
+      ViewData["Season"] = new SelectList(_context.Season, "Id", "Sport", round.Season);
+      ViewData["Venue"] = new SelectList(_context.Venue, "Name", "Name", round.Venue);
+      return View(round);
+    }
+
+    // GET: Rounds/Edit/5
+    public async Task<IActionResult> Edit(int? id) {
+      if (id == null) return NotFound();
+
+      var round = await _context.Round.SingleOrDefaultAsync(m => m.Id == id);
+      if (round == null) return NotFound();
+      ViewData["Season"] = new SelectList(_context.Season, "Id", "Sport", round.Season);
+      ViewData["Venue"] = new SelectList(_context.Venue, "Name", "Name", round.Venue);
+      return View(round);
+    }
+
+    // POST: Rounds/Edit/5
+    // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+    // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit(
+      int id,
+      [Bind("Id,Date,Number,Name,Season,Venue")]
+      Round round) {
+      if (id != round.Id) return NotFound();
+
+      if (ModelState.IsValid) {
+        try {
+          _context.Update(round);
+          await _context.SaveChangesAsync();
+        } catch (DbUpdateConcurrencyException) {
+          if (!RoundExists(round.Id)) return NotFound();
+          else throw;
+        }
+
+        return RedirectToAction(nameof(Index));
+      }
+
+      ViewData["Season"] = new SelectList(_context.Season, "Id", "Sport", round.Season);
+      ViewData["Venue"] = new SelectList(_context.Venue, "Name", "Name", round.Venue);
+      return View(round);
+    }
+
+    // GET: Rounds/Delete/5
+    public async Task<IActionResult> Delete(int? id) {
+      if (id == null) return NotFound();
+
+      var round = await _context.Round
+        .Include(r => r.RelatedSeason)
+        .Include(r => r.RelatedVenue)
+        .SingleOrDefaultAsync(m => m.Id == id);
+      if (round == null) return NotFound();
+
+      return View(round);
+    }
+
+    // POST: Rounds/Delete/5
+    [HttpPost]
+    [ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(int id) {
+      var round = await _context.Round.SingleOrDefaultAsync(m => m.Id == id);
+      _context.Round.Remove(round);
+      await _context.SaveChangesAsync();
+      return RedirectToAction(nameof(Index));
+    }
+
+    bool RoundExists(int id) {
+      return _context.Round.Any(e => e.Id == id);
+    }
+  }
+}
---
+++ src/Motorsports.Scaffolding.Core/Models/DisplayModels/RoundDisplayModel.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Motorsports.Scaffolding.Core.Models.EditModels;
+
+namespace Motorsports.Scaffolding.Core.Models.DisplayModels {
+  public class RoundDisplayModel : DisplayModel<Round> {
+    public RoundDisplayModel(
+      Round round,
+      IEnumerable<Season> seasons = null,
+      IEnumerable<Team> teams = null,
+      IEnumerable<Participant> participants = null,
+      IEnumerable<Status> statuses = null) : base(round) {
+      AvailableSeasons = seasons;
+      AvailableTeams = teams;
+      AvailableParticipants = participants;
+      AvailableStatuses = statuses;
+      WinningParticipantIds = round.RelatedRoundWinners?.Select(winner => winner.Participant.ToString()).ToArray() ?? Enumerable.Empty<string>().ToArray();
+    }
+
+    public int Id {
+      get => DataModel.Id;
+      set => DataModel.Id = value;
+    }
+
+    public string Name {
+      get => DataModel.Name;
+      set => DataModel.Name = value;
+    }
+
+    public DateTime Date {
+      get => DataModel.Date;
+      set => DataModel.Date = value;
+    }
+
+    public short Number {
+      get => DataModel.Number;
+      set => DataModel.Number = value;
+    }
+
+    public string Status => DataModel.RelatedRoundResult?.Status ?? RoundEditModel.RoundStatus.Scheduled.ToString();
+
+    public short? Rating => (short?) DataModel.RelatedRoundResult?.Rating;
+
+    public RoundEditModel.RainLevel? Rain => DataModel.RelatedRoundResult?.Rain.HasValue ?? false
+      ? Enum.Parse<RoundEditModel.RainLevel>(Enum.GetName(typeof(RoundEditModel.RainLevel), DataModel.RelatedRoundResult.Rain))
+      : new RoundEditModel.RainLevel?();
+
+    public Season RelatedSeason {
+      get => DataModel.RelatedSeason;
+      set => DataModel.RelatedSeason = value;
+    }
+
+    public Venue RelatedVenue {
+      get => DataModel.RelatedVenue;
+      set => DataModel.RelatedVenue = value;
+    }
+
+    public IEnumerable<Team> AvailableTeams { get; }
+    public IEnumerable<Participant> AvailableParticipants { get; }
+    public IEnumerable<Status> AvailableStatuses { get; }
+    public IEnumerable<Season> AvailableSeasons { get; }
+
+    [DisplayName("Winning team")]
+    public int? WinningTeamId {
+      get => DataModel.RelatedRoundResult?.WinningTeam;
+      set => DataModel.RelatedRoundResult = value.HasValue
+        ? new RoundResult {Round = DataModel.Id, WinningTeam = value.Value}
+        : null;
+    }
+
+    [DisplayName("Winning participant(s)")]
+    public string[] WinningParticipantIds { get; set; }
+
+    [DisplayName("Nice name")]
+    public string NiceName => $"{DataModel.RelatedSeason.Sport}: {Number} {RelatedVenue} ({Date:d MMM yyyy}) - {Status}";
+
+    [DisplayName("Winners")]
+    public string Winners {
+      get {
+        var winningTeam = DataModel.RelatedRoundResult?.RelatedWinningTeam;
+        var winningParticipants = (DataModel.RelatedRoundWinners?.Select(sw => sw.RelatedParticipant) ?? Enumerable.Empty<Participant>()).ToList();
+        if (!winningParticipants.Any() && winningTeam == null) return "/";
+        if (winningParticipants.Any() && winningParticipants.Any() && winningTeam == null) return $"{string.Join(", ", winningParticipants.Select(p => p.GetFullName()))}";
+        if (!winningParticipants.Any() && winningTeam != null) return $"{winningTeam.Name}";
+        return $"{string.Join(", ", winningParticipants.Select(p => p.GetFullName()))} ({winningTeam?.Name})";
+      }
+    }
+
+    [DisplayName("Winning team")]
+    public string WinningTeam => DataModel.RelatedRoundResult?.RelatedWinningTeam?.Name ?? "/";
+
+    [DisplayName("Winning participant(s)")]
+    public string WinningParticipants {
+      get {
+        var winningParticipants = (DataModel.RelatedRoundWinners?.Select(sw => sw.RelatedParticipant) ?? Enumerable.Empty<Participant>()).ToList();
+        return winningParticipants.Any()
+          ? string.Join(", ", winningParticipants)
+          : "/";
+      }
+    }
+  }
+}
