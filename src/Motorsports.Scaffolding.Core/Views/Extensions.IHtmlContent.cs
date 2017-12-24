fb73922ebe4aca7a25ad83b980451195b4693067
David Lievrouw <dlievrouw2@csc.com>
2017-12-24 15:41:16 +0100

    Make Create Round work.

---
+++ src/Motorsports.Scaffolding.Core/Views/Extensions.IHtmlContent.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using System.Text;
+using System.Text.Encodings.Web;
+using Microsoft.AspNetCore.Html;
+
+namespace Motorsports.Scaffolding.Core.Views {
+  public static partial class Extensions {
+    public static string ToHtmlString(this IHtmlContent htmlContent) {
+      var builder = new StringBuilder();
+      using (var output = new StringWriter(builder)) {
+        htmlContent.WriteTo(output, HtmlEncoder.Default);
+      }
+      return builder.ToString().Trim();
+    }
+  }
+}
---
+++ src/Motorsports.Scaffolding.Core/Views/Shared/EditorTemplates/Season.cshtml.cs
@@ -0,0 +1,7 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Motorsports.Scaffolding.Core.Views.Shared {
+  public class SeasonModel : PageModel {
+    public void OnGet() { }
+  }
+}
