a37f2bb16e313eae758430972c56d2c75168950f
David Lievrouw <dlievrouw2@csc.com>
2017-12-30 16:24:21 +0100

    Add validation for Venue.

---
+++ src/Motorsports.Scaffolding.Core/Models/Validators/Create/CreateVenueValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using FluentValidation;
+
+namespace Motorsports.Scaffolding.Core.Models.Validators.Create {
+  public class CreateVenueValidator : MotorsportsValidator<Venue>, ICreateValidator<Venue> {
+    readonly MotorsportsContext _context;
+
+    public CreateVenueValidator(MotorsportsContext context) {
+      _context = context ?? throw new ArgumentNullException(nameof(context));
+
+      RuleFor(_ => _.Name)
+        .NotEmpty()
+        .WithMessage("A name is required.");
+
+      RuleFor(_ => _.Country)
+        .NotEmpty()
+        .WithMessage("A country is required.");
+
+      RuleFor(_ => _.Name)
+        .Must(BeUnique)
+        .WithMessage("A venue with the specified name already exists.")
+        .When(_ => !string.IsNullOrEmpty(_.Name));
+    }
+
+    bool BeUnique(Venue venue, string name) {
+      return !_context.Venue.Any(_ => StringComparer.InvariantCultureIgnoreCase.Equals(_.Name, name));
+    }
+  }
+}
---
+++ src/Motorsports.Scaffolding.Core/Models/Validators/Update/UpdateVenueValidator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using FluentValidation;
+
+namespace Motorsports.Scaffolding.Core.Models.Validators.Update {
+  public class UpdateVenueValidator : MotorsportsValidator<Venue>, IUpdateValidator<Venue> {
+    readonly MotorsportsContext _context;
+
+    public UpdateVenueValidator(MotorsportsContext context) {
+      _context = context ?? throw new ArgumentNullException(nameof(context));
+
+      RuleFor(_ => _.Name)
+        .NotEmpty()
+        .WithMessage("A name is required.");
+
+      RuleFor(_ => _.Country)
+        .NotEmpty()
+        .WithMessage("A country is required.");
+
+      RuleFor(_ => _.Name)
+        .Must(MustExist)
+        .WithMessage("The venue to update does not exist.")
+        .When(_ => !string.IsNullOrEmpty(_.Name));
+    }
+
+    bool MustExist(Venue venue, string name) {
+      return _context.Venue.SingleOrDefault(_ => StringComparer.InvariantCultureIgnoreCase.Equals(_.Name, name)) != null;
+    }
+  }
+}
