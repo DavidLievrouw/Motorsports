184530360ab97db58f0e442919cc4e715114418f
David Lievrouw <dlievrouw2@csc.com>
2018-01-02 12:17:23 +0100

    Initial version of Cake.Frosting Publish target.

--- src/build/Lifetime.cs
+++ src/build/Lifetime.cs
@@ -1,11 +1,17 @@
+using System.Diagnostics.CodeAnalysis;
+using Build.Tasks;
 using Cake.Common.Diagnostics;
 using Cake.Core;
 using Cake.Frosting;
 
 // ReSharper disable once CheckNamespace
 public sealed class Lifetime : FrostingLifetime<Context> {
+  [SuppressMessage("ReSharper", "InconsistentNaming")]
+  public static _Props _Props { get; private set; }
+
   public override void Setup(Context context) {
     context.Information("Setting things up...");
+    _Props = new _Props(context);
   }
 
   public override void Teardown(Context context, ITeardownContext info) {
--- src/build/Tasks/Publish.cs
+++ src/build/Tasks/Publish.cs
@@ -1,11 +1,31 @@
-using Cake.Common.Diagnostics;
+using Cake.Common.IO;
+using Cake.Common.Tools.DotNetCore;
+using Cake.Common.Tools.DotNetCore.Clean;
+using Cake.Common.Tools.DotNetCore.Publish;
+using Cake.Core;
 using Cake.Frosting;
 
 namespace Build.Tasks {
-  [TaskName("Publish")]
+  [TaskName(nameof(Publish))]
+  [Dependency(typeof(InitVersion))]
   public sealed class Publish : FrostingTask<Context> {
     public override void Run(Context context) {
-      context.Information("Publish!");
+      context.CleanDirectory(Lifetime._Props.PublishTargetDirectory);
+      context.DotNetCoreRestore(Lifetime._Props.ScaffoldingProjectFile.FullPath);
+      context.DotNetCoreClean(
+        Lifetime._Props.ScaffoldingProjectFile.FullPath,
+        new DotNetCoreCleanSettings {
+          Verbosity = Lifetime._Props.DotNetCoreVerbosity,
+          Configuration = Lifetime._Props.Configuration
+        });
+      context.DotNetCorePublish(
+        Lifetime._Props.ScaffoldingProjectFile.FullPath,
+        new DotNetCorePublishSettings {
+          Configuration = Lifetime._Props.Configuration,
+          OutputDirectory = Lifetime._Props.PublishTargetDirectory,
+          Verbosity = Lifetime._Props.DotNetCoreVerbosity,
+          ArgumentCustomization = args => args.Append("--no-restore")
+        });
     }
   }
 }
