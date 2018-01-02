9c62040507475e4031c42273c8e8209faf29fab5
David Lievrouw <dlievrouw2@csc.com>
2018-01-02 10:48:21 +0100

    Add authentication scheme to logged in principal identity.

--- src/Motorsports.Scaffolding.Core/Startup.cs
+++ src/Motorsports.Scaffolding.Core/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Authentication.Cookies;
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
@@ -68,7 +69,8 @@ namespace Motorsports.Scaffolding.Core {
         new UserDataService(provider.GetRequiredService<IQueryExecutor>()), 
         new RandomHashedPasswordProvider(provider.GetRequiredService<IHashPasswordService>()),
         provider.GetRequiredService<IHashPasswordService>(),
-        new UsernamePasswordCredentialsValidator()));
+        new UsernamePasswordCredentialsValidator(),
+        provider.GetRequiredService<IAuthenticationSchemeProvider>()));
 
       // Authentication
       services
