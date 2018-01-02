4e19576abd808b77926824788f8542cadd509fac
David Lievrouw <dlievrouw2@csc.com>
2018-01-02 19:56:23 +0100

    Move wait statement of StopIISApplicationPool task to Finally.

--- src/Cake.Frosting/Tasks/StopIISApplicationPoolIfExists.cs
+++ src/Cake.Frosting/Tasks/StopIISApplicationPoolIfExists.cs
@@ -1,4 +1,5 @@
 using System;
+using Cake.Common.Diagnostics;
 using Cake.Core;
 using Cake.Frosting;
 using Cake.IIS;
@@ -8,14 +9,17 @@ namespace Build.Tasks {
   public sealed class StopIISApplicationPoolIfExists : FrostingTaskWithProps<IISApplicationProps> {
     public override bool ShouldRun(ICakeContext context) {
       var props = GetProperties(context);
-
       return context.PoolExists(props.IISApplicationPoolSettings.Name);
     }
     
     public override void Run(ICakeContext context) {
       var props = GetProperties(context);
-
       context.StopPool(props.IISApplicationPoolSettings.Name);
+    }
+
+    public override void Finally(ICakeContext context) {
+      base.Finally(context);
+      context.Information("Waiting for application pool to gracefully quit...");
       System.Threading.Tasks.Task.Delay(TimeSpan.FromSeconds(4)).Wait(); // Make sure it is stopped
     }
   }
