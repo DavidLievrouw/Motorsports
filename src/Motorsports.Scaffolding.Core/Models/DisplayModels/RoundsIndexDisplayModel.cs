d715a6a3db3d75bb8eb5473991daa89173ddb578
David Lievrouw <dlievrouw2@csc.com>
2018-01-04 10:01:53 +0100

    When entering the Rounds Index view, automatically scroll to the bottom.

--- src/Motorsports.Scaffolding.Core/Models/DisplayModels/RoundsIndexDisplayModel.cs
+++ src/Motorsports.Scaffolding.Core/Models/DisplayModels/RoundsIndexDisplayModel.cs
@@ -4,5 +4,6 @@ namespace Motorsports.Scaffolding.Core.Models.DisplayModels {
   public class RoundsIndexDisplayModel {
     public Season Season { get; set; }
     public IEnumerable<RoundDisplayModel> Rounds { get; set; }
+    public RoundDisplayModel LastRound { get; set; }
   }
 }
