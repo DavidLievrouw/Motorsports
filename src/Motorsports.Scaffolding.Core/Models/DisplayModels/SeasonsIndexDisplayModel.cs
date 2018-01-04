8185cbda273eb18652cc372b9a2858026fa31359
David Lievrouw <dlievrouw2@csc.com>
2018-01-04 09:55:52 +0100

    Group the seasons in the Season Index view per sport, and order them by start date descending.

---
+++ src/Motorsports.Scaffolding.Core/Models/DisplayModels/SeasonsIndexDisplayModel.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace Motorsports.Scaffolding.Core.Models.DisplayModels {
+  public class SeasonsIndexDisplayModel {
+    public Dictionary<Sport, IEnumerable<SeasonDisplayModel>> SeasonsPerSport { get; set; }
+  }
+}
