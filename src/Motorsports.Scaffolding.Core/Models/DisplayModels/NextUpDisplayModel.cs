8790fb4c59e5695531486653c153bc386dfd817a
David Lievrouw <dlievrouw2@csc.com>
2018-01-04 11:39:47 +0100

    Overhaul of the NextUp items on the Home view.

--- src/Motorsports.Scaffolding.Core/Models/DisplayModels/NextUpDisplayModel.cs
+++ src/Motorsports.Scaffolding.Core/Models/DisplayModels/NextUpDisplayModel.cs
@@ -1,14 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Motorsports.Scaffolding.Core.Models.DisplayModels {
   public class NextUpDisplayModel {
-    public NextUpDisplayModel(NextUp nextUp) {
+    public NextUpDisplayModel(NextUp nextUp, IEnumerable<NextUp> allRoundsNextUp) {
       Id = nextUp.Id;
       Sport = nextUp.Sport;
       Number = nextUp.Number;
       Date = nextUp.Date;
       Name = nextUp.Name;
       Venue = nextUp.Venue;
+      IsVeryNextUp = allRoundsNextUp
+                       .Where(n => n.Date.Date <= DateTime.Now.Date)
+                       .OrderBy(n => n.Date)
+                       .FirstOrDefault() == nextUp;
     }
 
     public int Id { get; set; }
@@ -17,6 +23,7 @@ namespace Motorsports.Scaffolding.Core.Models.DisplayModels {
     public DateTime Date { get; set; }
     public string Name { get; set; }
     public string Venue { get; set; }
+    public bool IsVeryNextUp { get; set; }
 
     public bool IsInPast => Date.Date <= DateTime.Now.Date;
 
