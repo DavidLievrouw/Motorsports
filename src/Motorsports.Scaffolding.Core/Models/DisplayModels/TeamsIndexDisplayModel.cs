22a7989a65316a928b89f310c072b850adb18604
David Lievrouw <dlievrouw2@csc.com>
2018-01-07 16:51:32 +0100

    Group items in Teams Index view per Sport.

---
+++ src/Motorsports.Scaffolding.Core/Models/DisplayModels/TeamsIndexDisplayModel.cs
@@ -0,0 +1,7 @@
+﻿using System.Collections.Generic;
+
+namespace Motorsports.Scaffolding.Core.Models.DisplayModels {
+  public class TeamsIndexDisplayModel {
+    public Dictionary<Sport, IEnumerable<Team>> TeamsPerSport { get; set; }
+  }
+}
