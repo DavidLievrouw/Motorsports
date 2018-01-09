9e4abe5ea826b220ba289b95a2463fb3a2e3949d
David Lievrouw <dlievrouw2@csc.com>
2018-01-09 16:06:46 +0100

    Make sure that the start and end date of seasons are correctly calculated in the SeasonDisplayModel.

--- src/Motorsports.Scaffolding.Core/Models/DisplayModels/SeasonDisplayModel.cs
+++ src/Motorsports.Scaffolding.Core/Models/DisplayModels/SeasonDisplayModel.cs
@@ -49,11 +49,11 @@ namespace Motorsports.Scaffolding.Core.Models.DisplayModels {
 
     [DisplayName("Start date")]
     [DisplayFormat(DataFormatString = "{0:d MMM yyyy}", NullDisplayText = "/")]
-    public DateTime? StartDate => DataModel.RelatedRounds?.Min(r => r.Date);
+    public DateTime? StartDate => DataModel.RelatedRounds?.MinOrDefault(r => r.Date);
 
     [DisplayName("End date")]
     [DisplayFormat(DataFormatString = "{0:d MMM yyyy}", NullDisplayText = "/")]
-    public DateTime? EndDate => DataModel.RelatedRounds?.Max(r => r.Date);
+    public DateTime? EndDate => DataModel.RelatedRounds?.MaxOrDefault(r => r.Date);
 
     public IEnumerable<Team> AvailableTeams { get; }
     public IEnumerable<Participant> AvailableParticipants { get; }
