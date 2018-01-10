d98c2087fedb8e8f814d8a27c2f4abfe92b26f1b
David Lievrouw <dlievrouw2@csc.com>
2018-01-10 14:37:29 +0100

    Make Create SeasonEntry work.

--- src/Motorsports.Scaffolding.Core/Models/SeasonEntry.cs
+++ src/Motorsports.Scaffolding.Core/Models/SeasonEntry.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 
 namespace Motorsports.Scaffolding.Core.Models {
   public partial class SeasonEntry {
@@ -13,5 +14,37 @@ namespace Motorsports.Scaffolding.Core.Models {
 
     [DisplayName("Team")]
     public Team RelatedTeam { get; set; }
+
+    public class SeasonEntryKey : IEquatable<SeasonEntryKey> {
+      public int Season { get; set; }
+      public int Team { get; set; }
+
+      public bool Equals(SeasonEntryKey other) {
+        if (ReferenceEquals(null, other)) return false;
+        if (ReferenceEquals(this, other)) return true;
+        return Season == other.Season && Team == other.Team;
+      }
+
+      public override bool Equals(object obj) {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        var other = obj as SeasonEntryKey;
+        return other != null && Equals(other);
+      }
+
+      public override int GetHashCode() {
+        unchecked {
+          return (Season * 397) ^ Team;
+        }
+      }
+
+      public static bool operator ==(SeasonEntryKey left, SeasonEntryKey right) {
+        return Equals(left, right);
+      }
+
+      public static bool operator !=(SeasonEntryKey left, SeasonEntryKey right) {
+        return !Equals(left, right);
+      }
+    }
   }
 }
---
+++ src/Motorsports.Scaffolding.Core/Models/Validators/Create/CreateSeasonEntryValidator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+using FluentValidation;
+
+namespace Motorsports.Scaffolding.Core.Models.Validators.Create {
+  public class CreateSeasonEntryValidator : MotorsportsValidator<SeasonEntry, SeasonEntry.SeasonEntryKey>, ICreateValidator<SeasonEntry> {
+    readonly MotorsportsContext _context;
+
+    public CreateSeasonEntryValidator(MotorsportsContext context) {
+      _context = context ?? throw new ArgumentNullException(nameof(context));
+
+      RuleFor(_ => _.Season)
+        .Must(SeasonExists)
+        .WithMessage("The specified season does not exist.");
+
+      RuleFor(_ => _.Team)
+        .Must(BeUnique)
+        .WithMessage("An entry for this season and team already exists.")
+        .Must(TeamExists)
+        .WithMessage("The specified team does not exist.");
+
+      RuleFor(_ => _.Name)
+        .NotEmpty()
+        .WithMessage("A name is required.");
+    }
+
+    bool SeasonExists(SeasonEntry seasonEntry, int season) {
+      return _context.Season.Any(_ => _.Id == season);
+    }
+    
+    bool TeamExists(SeasonEntry seasonEntry, int team) {
+      return _context.Team.Any(_ => _.Id == team);
+    }
+
+    bool BeUnique(SeasonEntry seasonEntry, int season) {
+      return !_context.SeasonEntry.Any(_ => 
+        _.Season == seasonEntry.Season && 
+        _.Team == seasonEntry.Team);
+    }
+  }
+}
