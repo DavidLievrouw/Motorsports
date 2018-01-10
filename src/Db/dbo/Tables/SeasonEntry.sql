a7b6622f211f9bdc98195d44f98605a7fa2ef6f0
David Lievrouw <dlievrouw2@csc.com>
2018-01-10 09:43:26 +0100

    Create and perform initial load for the SeasonEntry database table.

---
+++ src/Db/dbo/Tables/SeasonEntry.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[SeasonEntry] (
+  [SeasonId] INT NOT NULL, 
+  [TeamId] INT NOT NULL, 
+  [Name] NVARCHAR(50) NOT NULL, 
+  PRIMARY KEY ([SeasonId], [TeamId]), 
+  CONSTRAINT [FK_Season_SeasonEntry] FOREIGN KEY ([SeasonId]) REFERENCES [dbo].[Season]([Id]) ON DELETE CASCADE,
+  CONSTRAINT [FK_Team_SeasonEntry] FOREIGN KEY ([TeamId]) REFERENCES [dbo].[Team]([Id])
+)
