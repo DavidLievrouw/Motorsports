47b6ddaf3580858ea333efde09f2e4339db56928
David Lievrouw <dlievrouw2@csc.com>
2018-01-11 12:07:01 +0100

    Implement delete validation for Season and Round.

--- src/Motorsports.Scaffolding.Core/Models/Validators/Delete/DisallowDeleteInGeneralValidator.cs
+++ src/Motorsports.Scaffolding.Core/Models/Validators/Delete/DisallowDeleteInGeneralValidator.cs
@@ -2,27 +2,17 @@
 using System.Threading.Tasks;
 
 namespace Motorsports.Scaffolding.Core.Models.Validators.Delete {
-  public class DisallowDeleteValidator
+  public class DisallowDeleteInGeneralValidator
     : IDeleteValidator<Sport>,
       IDeleteValidator<Venue>,
       IDeleteValidator<Team>,
-      IDeleteValidator<Participant>,
-      IDeleteValidator<Round>,
-      IDeleteValidator<Season> {
+      IDeleteValidator<Participant> {
     public Task<ValidationResult> ValidateForDeleteAsync(Participant instance, CancellationToken cancellationToken = default(CancellationToken)) {
       return Task.FromResult(
         new ValidationResult(
           new ValidationFailure(
             nameof(instance.Id),
             $"Deleting this {nameof(Participant)} is not allowed.")));
-    }
-
-    public Task<ValidationResult> ValidateForDeleteAsync(Season instance, CancellationToken cancellationToken = default(CancellationToken)) {
-      return Task.FromResult(
-        new ValidationResult(
-          new ValidationFailure(
-            nameof(instance.Id),
-            $"Deleting this {nameof(Season)} is not allowed.")));
     }
 
     public Task<ValidationResult> ValidateForDeleteAsync(Sport instance, CancellationToken cancellationToken = default(CancellationToken)) {
@@ -47,14 +37,6 @@ namespace Motorsports.Scaffolding.Core.Models.Validators.Delete {
           new ValidationFailure(
             nameof(instance.Name),
             $"Deleting this {nameof(Venue)} is not allowed.")));
-    }
-
-    public Task<ValidationResult> ValidateForDeleteAsync(Round instance, CancellationToken cancellationToken = default(CancellationToken)) {
-      return Task.FromResult(
-        new ValidationResult(
-          new ValidationFailure(
-            nameof(instance.Id),
-            $"Deleting this {nameof(Round)} is not allowed.")));
     }
   }
 }
