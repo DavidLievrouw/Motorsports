912ed94897db0866564bb9a13ae985ca114c863e
David Lievrouw <David.lievrouw@recomatics.com>
2018-07-26 11:58:04 +0200

    Add a dedicated PublishNoUI.cmd for CI

--- PublishNoUI.cmd
+++ PublishNoUI.cmd
@@ -8,5 +8,3 @@ cd %~dp0
 TITLE Motorsports -- Publish
 cd ./src/Build
 Powershell.exe -File build.ps1 -Target Publish -Configuration Release -PublishEnvironment Production -Verbosity Normal -SkipInstallDotNetCoreCli True
-CHOICE /T 60 /C yYnN /CS /D y  /M "Should this window close? [Default y, you have 60 seconds]:"
-if errorlevel 2 pause
