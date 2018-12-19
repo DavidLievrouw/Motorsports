60f1b830cc5fd4e07e4dbbb24357bc4d6703461a
David Lievrouw <David.lievrouw@recomatics.com>
2018-12-19 16:57:46 +0100

    Remove test file again

--- PublishCI.cmd
+++
@@ -1,3 +0,0 @@
-REM Remove this file when Azure DevOps supports Cake.Frosting by nuget.org
-TITLE Monitoring -- Publish
-dotnet run --project "./src/Build/Build.csproj" --target Publish --configuration Release --no-launch-profile
