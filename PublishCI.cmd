a444824de36e402e01abff229354b7b44c0600ad
David Lievrouw <David.lievrouw@recomatics.com>
2018-12-19 16:54:48 +0100

    Add cmd file to be called by Azure DevOps

---
+++ PublishCI.cmd
@@ -0,0 +1,3 @@
+REM Remove this file when Azure DevOps supports Cake.Frosting by nuget.org
+TITLE Monitoring -- Publish
+dotnet run --project "./src/Build/Build.csproj" --target Publish --configuration Release --no-launch-profile --no-restore
