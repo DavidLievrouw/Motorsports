7626ebea2538d42caa163372e79548bd3589a86b
David Lievrouw <david.lievrouw@recomatics.com>
2024-10-21 13:44:58 +0200

    Update dockerfile.

--- Docker_Run.cmd
+++ Docker_Run.cmd
@@ -7,7 +7,7 @@ docker stop motorsports
 docker rm motorsports
 docker image rm motorsports
 docker build -t motorsports src\Motorsports.Scaffolding.Core
-docker run -d -p 8080:80 --name motorsports motorsports
+docker run -d -p 8080:80 -e Kestrel:Endpoints:Http:Url=http://+:80 -e ConnectionStrings:Motorsports="Server=docker.host.internal;Database=Motorsports;user id=sa;password=s3cr3t;TrustServerCertificate=Yes;" --name motorsports motorsports
 ping 127.0.0.1 -n 4 > nul 
 start "" http://localhost:8080
 CHOICE /T 60 /C yYnN /CS /D y  /M "Should this window close? [Default y, you have 60 seconds]:"
